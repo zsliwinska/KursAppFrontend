48ca63cccde7a2e9dd27d1887d4dccb12acaa9ce
Zuza Sliwinska <z.sliwinska98@gmail.com>
2023-04-05 14:20:38 +0200

    praca domowa 3.3

--- src/app/modules/admin/admin-product/admin-product.component.ts
+++ src/app/modules/admin/admin-product/admin-product.component.ts
@@ -16,7 +16,7 @@ export class AdminProductComponent implements  AfterViewInit {
   // dataSource: AdminProduct[] = [];
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatTable) table!: MatTable<any>;
-  displayedColumns: string[] = ["id", "name", "price","actions"];
+  displayedColumns: string[] = ["image", "id", "name", "price","actions"];
   totalElements: number = 0;
   data: AdminProduct[] = [];
 
